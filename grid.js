cb51384d0d8d20f83912e7e359d9a764962dc612
Jonathan Santos <bitfut@gmail.com>
2022-05-26 17:18:22 +0200

    grid function and css borders parametrized

--- grid.js
+++ grid.js
@@ -1,27 +1,26 @@
-function makeGrid()
+function makeGrid(rows, columns)
 {
-    let table = document.getElementById("grid")
-    let navbarHeight = document.getElementById("navbar").clientHeight
+    let table = document.getElementById('grid')
+    let navbarHeight = document.getElementById('navbar').clientHeight
 
     let width = window.innerWidth
     let height = window.innerHeight
 
-    table.style.width = width + "px";
-    table.style.height = height - navbarHeight + "px";
+    table.style.width = width + 'px'
+    table.style.height = height - navbarHeight + 'px'
 
-    for(let i = 0; i < 10; i++)
+    for(let i = 0; i < rows; i++)
     {
         let row = document.createElement("tr")
-        row.id = "row" + i
-        row.style.height = window.innerHeight/10
-
+        row.id = (i + 1 != rows) ? 'row' + i : 'lastrow'
+        row.style.height = window.innerHeight/rows
         table.appendChild(row)
-        row = document.getElementById("row" + i)
 
-        for(let i = 0; i < 10; i++)
+        for(let i = 0; i < columns; i++)
         {
-            let cell = document.createElement("td")
-            cell.style.width = window.innerWidth/10
+            let cell = document.createElement('td')
+            cell.id = (i + 1 != columns) ? 'cell' + i : 'lastcell' 
+            cell.style.width = window.innerWidth/columns
             row.appendChild(cell)
         }
     }
