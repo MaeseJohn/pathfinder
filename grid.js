b57c13efd247e6874767d1d0b9f11eae792d0a7a
Jonathan Santos <bitfut@gmail.com>
2022-06-01 11:42:15 +0200

    css

--- grid.js
+++ grid.js
@@ -4,25 +4,19 @@ function makeGrid(rows, columns)
     
     let sideSizes
     sideSizes = gridSize(rows, columns)
-    table.style.width = sideSizes.width + 'px'
+    table.style.width  = sideSizes.width + 'px'
     table.style.height = sideSizes.height + 'px'
 
     for(let i = 0; i < rows; i++)
     {
         let row = document.createElement("tr")
-
-        //Chage last colum id to made it black with css 
-        row.id = (i + 1 != rows) ? 'row' + i : 'lastrow'
-
+        row.id  = 'row' + i 
         table.appendChild(row)
 
         for(let i = 0; i < columns; i++)
         {
             let cell = document.createElement('td')
-            
-            //Chage last row id to made it black with css 
-            cell.id = (i + 1 != columns) ? 'cell' + i : 'lastcell' 
-
+            cell.id  =  'cell' + i  
             row.appendChild(cell)
         }
     }
